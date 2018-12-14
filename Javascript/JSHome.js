721a9dfaec0ed7c25183b3447e052cb8ba452b8e
GagasW <kurnia.walandri@binus.ac.id>
2018-12-14 21:52:11 +0700

    Push1

---
+++ Javascript/JSHome.js
@@ -0,0 +1,28 @@
+function DropdownMenu() {
+	document.getElementById('dropdownContainer').classList.toggle("show");
+}
+
+//buat bikin jam mau gua update lagi jgn di apa apain wkwk
+// function updateClock() {
+//     var now = new Date();
+//         months = ['January', 'February', 'March','April','May','June','July','August','September','October','November','December'];
+//         time = now.getHours() + ':' + now.getMinutes();
+
+//         date = [now.getDate(), 
+//                 months[now.getMonth()],
+//                 now.getFullYear()].join(' ');
+
+//     document.getElementById('time').innerHTML = [date, time].join(' / ');
+
+//     setTimeout(updateClock, 1000);
+// }
+// updateClock();
+
+window.onclick = function(trigger){
+	if (!trigger.target.matches('.dropbtn')) {
+		var dropdownContainer = document.getElementById('dropdownContainer');
+		if(dropdownContainer.classList.contains('show')){
+			dropdownContainer.classList.remove('show');
+		}
+	}
+}
