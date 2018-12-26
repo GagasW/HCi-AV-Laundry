c26fb7b60e90fbb2f9f1c0f1b6e58d60d9763027
GagasW <kurnia.walandri@binus.ac.id>
2018-12-26 19:58:44 +0700

    asasasa
    asasasas

--- Javascript/JSHome.js
+++ Javascript/JSHome.js
@@ -2,21 +2,21 @@ function DropdownMenu() {
 	document.getElementById('dropdownContainer').classList.toggle("show");
 }
 
-//buat bikin jam mau gua update lagi jgn di apa apain wkwk
-// function updateClock() {
-//     var now = new Date();
-//         months = ['January', 'February', 'March','April','May','June','July','August','September','October','November','December'];
-//         time = now.getHours() + ':' + now.getMinutes();
+buat bikin jam mau gua update lagi jgn di apa apain wkwk
+function updateClock() {
+    var now = new Date();
+        months = ['January', 'February', 'March','April','May','June','July','August','September','October','November','December'];
+        time = now.getHours() + ':' + now.getMinutes();
 
-//         date = [now.getDate(), 
-//                 months[now.getMonth()],
-//                 now.getFullYear()].join(' ');
+        date = [now.getDate(), 
+                months[now.getMonth()],
+                now.getFullYear()].join(' ');
 
-//     document.getElementById('time').innerHTML = [date, time].join(' / ');
+    document.getElementById('times').innerHTML = [date, time].join(' / ');
 
-//     setTimeout(updateClock, 1000);
-// }
-// updateClock();
+    setTimeout(updateClock, 1000);
+}
+updateClock();
 
 window.onclick = function(trigger){
 	if (!trigger.target.matches('.dropbtn')) {
