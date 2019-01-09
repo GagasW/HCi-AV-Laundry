27ec6069a01cd4a82c1287e2e091a24b28032526
SonKatarina23 <houwenlie98@gmail.com>
2019-01-09 16:56:46 +0700

    Slider

--- Javascript/JSGallery.js
+++ Javascript/JSGallery.js
@@ -1,4 +1,4 @@
-var i = 0
+/*var i = 0
 var images = [];
 var time = 3000;
 
@@ -19,4 +19,38 @@ function changeImg() {
 	setTimeout("changeImg()", time);
 }
 
-window.onload = changeImg();
+window.onload = changeImg();*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+
+
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+/*window.onload = currentSlide(1);*/
